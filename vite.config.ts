9bc82732f2357a914ff8c8fe7cbbb1f7e42c3d8c
tunghoang <tunght@goonus.io>
2025-04-11 08:11:34 +0700

    deep clone margin tier

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the mode (development or production)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    define: {
+      // Pass environment variables to the client
+      "process.env": env,
+    },
+    build: {
+      outDir: mode === "production" ? "dist/prod" : "dist/dev", // Separate output folders
+    },
+  };
 });
